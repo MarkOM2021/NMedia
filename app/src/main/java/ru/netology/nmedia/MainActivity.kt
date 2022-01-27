e3678bc152640d2f23f16ad0f1fb1f5f7025c127
MarkOM2021 <pymakarov@gmail.com>
2022-01-27 14:30:24 +0300

    The post UI has been constructed

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println(resources.displayMetrics.heightPixels) // 1794
+        println(resources.displayMetrics.widthPixels) // 1080
+        println(resources.displayMetrics.densityDpi) // 420
+        println(resources.displayMetrics.density) // 2.625
     }
 }
