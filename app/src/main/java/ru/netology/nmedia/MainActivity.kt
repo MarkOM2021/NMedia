42c1334ffed37147af2d34ece0eff2902d89216e
MarkOM2021 <pymakarov@gmail.com>
2022-01-29 12:20:08 +0300

    The views like and share edited to be able activate at listener action

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,14 +2,84 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import java.text.DecimalFormat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        println(resources.displayMetrics.heightPixels) // 1794
-        println(resources.displayMetrics.widthPixels) // 1080
-        println(resources.displayMetrics.densityDpi) // 420
-        println(resources.displayMetrics.density) // 2.625
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            likes = 1_199,
+            shares = 999_998,
+            likedByMe = false
+        )
+        with(binding) {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+            if (post.likedByMe) {
+                like?.setImageResource(R.drawable.ic_liked_24)
+            }
+            likeCount?.text = post.likes.toString()
+
+            root.setOnClickListener {
+                Log.d("stuff", "BindRoot")
+            }
+
+            like.setOnClickListener {
+                Log.d("stuff", "BindLike")
+            }
+
+            avatar.setOnClickListener {
+                Log.d("stuff", "avatar")
+            }
+
+            like?.setOnClickListener {
+                Log.d("stuff", "like")
+                post.likedByMe = !post.likedByMe
+                like.setImageResource(
+                    if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
+                )
+                if (post.likedByMe) post.likes++ else post.likes--
+                likeCount?.text = eventNumberFormatter(post.likes)
+            }
+
+            share?.setOnClickListener {
+                Log.d("stuff", "share")
+                post.shares++
+                shareCount?.text = eventNumberFormatter(post.shares)
+            }
+        }
+    }
+}
+
+fun eventNumberFormatter (num: Int):String {
+    val numDouble = num.toDouble()
+    val df = DecimalFormat("#.#")
+    when (num) {
+        in 0..1_100 -> {
+            return "$num"
+        }
+        in 1_100..10_000 -> {
+            val rounded = df.format(numDouble/1_000)
+            return rounded + "K"
+        }
+        in 10001..999999 -> {
+            val roundedK = num/1_000
+            return "$roundedK" + "K"
+        }
+        else -> {
+            val roundedM = df.format(numDouble/1_000_000)
+            return roundedM + "M"
+        }
     }
 }
